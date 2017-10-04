cc14c0d371c9d45957bc16c071f1ceb1341d29b8
nayeli cruz <nayelicruz12@gmail.com>
2017-10-04 19:56:34 -0400

    added routes and added welcome controller and views

---
+++ DrankApp/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :drinks
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root '/welcome#index'
+  get '/welcome', to: 'welcome#index'
+  resources :drinks
+end
